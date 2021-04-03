bf7dcf6c6b6b4b7ad7202ada499f8a81227a6c14
Jacob Adams <jaadams01@email.wm.edu>
2021-04-02 22:13:14 -0400

    sample_autocomplete: Remove uneeded sleep and sleep import

--- pycurses.py
+++ pycurses.py
@@ -1,5 +1,4 @@
 import curses
-from time import sleep
 
 def defaultwinwidth():
     return curses.COLS // 2
@@ -26,7 +25,6 @@ def formtest(win):
     win.getkey()
 
 def sample_autocomplete(s, limit):
-    #sleep(1)
     lorum = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum".lower().split()
     ret = []
     for t in lorum:
